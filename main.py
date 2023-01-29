0369bd19c8c7aa324603b85f5541c15624bf5ad7
Adeesh M Shrestha <35621337+adeeshmans@users.noreply.github.com>
2023-01-29 20:08:09 +0000

    format

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ print("")
 
 # Get input and convert string to float / store in var
 bill = float(input("What was your total bill? $"))
+
 # Get input and convert string to int / store in var
 tip = int(input("How much tip would you like to give? 10, 12,or 15? "))
+
 # Get input and convert string to int / store in var
 people = int(input("How many people would you like to split the bill with? "))
 
@@ -17,6 +19,7 @@ bill_per_person = total_bill / people
 
 # Final amount below rounds to 2 decimal points for certain bill amount
 final_amount = round(bill_per_person, 2)
+
 # This Final amount is more accurate for rounding to 2 dec points
 final_amount ="{:.2f}".format(bill_per_person)
 
