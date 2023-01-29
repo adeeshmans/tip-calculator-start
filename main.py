14ddc532e63ee9ae421fed4d92e9296497a79e11
Adeesh M Shrestha <35621337+adeeshmans@users.noreply.github.com>
2023-01-29 20:07:08 +0000

    Tip calculator completed, with the formula tip_percent = tip / 100 total_tip_amount = bill * tip_percent total_bill = bill + total_tip_amount bill_per_person = total_bill / people and rounding the results to 2 decimal places.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-#If the bill was $150.00, split between 5 people, with 12% tip. 
 
-#Each person should pay (150.00 / 5) * 1.12 = 33.6
-#Format the result to 2 decimal places = 33.60
+print("Welcome to the Tip Calculator")
+print("")
 
-#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+# Get input and convert string to float / store in var
+bill = float(input("What was your total bill? $"))
+# Get input and convert string to int / store in var
+tip = int(input("How much tip would you like to give? 10, 12,or 15? "))
+# Get input and convert string to int / store in var
+people = int(input("How many people would you like to split the bill with? "))
 
-#Write your code below this line 👇
+# Formula
+tip_percent = tip / 100
+total_tip_amount = bill * tip_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+
+# Final amount below rounds to 2 decimal points for certain bill amount
+final_amount = round(bill_per_person, 2)
+# This Final amount is more accurate for rounding to 2 dec points
+final_amount ="{:.2f}".format(bill_per_person)
+
+print(f"Each person pays: ${final_amount}")
